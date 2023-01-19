2f05a6684c098f28aeb50ac865db69a74120c3a7
Darya Shlyakhovaya <dshlyakhovaya1992@gmail.com>
2023-01-19 16:48:37 +0200

    add hw

--- controllers/contacts/favoriteContact.js
+++ controllers/contacts/favoriteContact.js
@@ -1,18 +1,20 @@
 const { Contact } = require("../../models/contact");
 const { NotFound } = require("http-errors");
 
-const updateContact = async (req, res, next) => {
+const updateFavoriteStatus = async (req, res, next) => {
   try {
     const { contactId } = req.params;
     const result = await Contact.findByIdAndUpdate(contactId, req.body, {
       new: true,
     });
+
     if (!result) {
-      throw new NotFound(`Contact with id=${contactId} not found`);
+      throw new NotFound(404);
     }
-    res.json(result);
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
 };
-module.exports = updateContact;
+
+module.exports = updateFavoriteStatus;
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+mongoose.set("strictQuery", true);
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+const DB = process.env.MONGO_URL;
+const Port = process.env.PORT;
+console.log(DB);
+mongoose
+  .connect(DB, {
+    usenewurlparser: true,
+    useunifiedtopology: true,
+  })
+  .then(() => {
+    app.listen(Port);
+    console.log("Successfully connected ");
+  })
+  .catch((error) => {
+    console.log(`can not connect to database, ${error}`);
+    process.exit(1);
+  });
